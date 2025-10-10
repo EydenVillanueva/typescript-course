80c51c76386f02520eb1bb4092c26f7fe05d9490
eydenv <eydenv85@gmail.com>
2025-10-09 20:50:14 -0500

    basic classes syntax lecture

---
+++ features/classes.ts
@@ -0,0 +1,28 @@
+class Vehicle {
+  protected honk(): void {
+    console.log('honk honk');
+  }
+
+  constructor(public color: string) {}
+}
+
+const vehicle = new Vehicle('oracle');
+console.log(vehicle.color);
+
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
